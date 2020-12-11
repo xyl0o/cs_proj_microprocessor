846a32dacbc8b11f63ba414f321116cc59e34c52
Maximilian Bauregger <maximilian.bauregger@studium.uni-hamburg.de>
2020-12-11 03:03:57 +0100

    Add twos_comp() to util.py

--- util.py
+++ util.py
@@ -16,3 +16,10 @@ def set_flag(flag_reg: bytes, flag:str, value: bool) -> bytes:
 def get_flag(flag_reg: bytes, flag:str) -> bool:
     shift_amount = reversed(['OF', 'CARRY','CMP']).index(flag)
     return bool(flag_reg>>shift_amount & 1)
+
+# https://stackoverflow.com/questions/1604464/twos-complement-in-python
+def twos_comp(val, bits):
+    """compute the 2's complement of int value val"""
+    if (val & (1 << (bits - 1))) != 0:
+        val = val - (1 << bits)
+    return val
