d831200334e4ab92a271195ac110f97f31e3b519
Maximilian Bauregger <maximilian.bauregger@studium.uni-hamburg.de>
2021-01-14 17:07:25 +0100

    Add newline to end of adder.vhdl

--- vhdl/adder.vhdl
+++ vhdl/adder.vhdl
@@ -1,7 +1,12 @@
 entity adder is
   -- `i0`, `i1`, and the carry-in `ci` are inputs of the adder.
   -- `s` is the sum output, `co` is the carry-out.
-  port (i0, i1 : in bit; ci : in bit; s : out bit; co : out bit);
+  port (
+  	i0, i1 : in bit;
+  	ci : in bit;
+  	s : out bit;
+  	co : out bit
+  );
 end adder;
 
 architecture rtl of adder is
