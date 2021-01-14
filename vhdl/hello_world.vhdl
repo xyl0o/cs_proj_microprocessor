1a5f9a99045c9bee39dee96948928a2799c7182e
Maximilian Bauregger <maximilian.bauregger@studium.uni-hamburg.de>
2021-01-14 16:50:45 +0100

    Add hello_world.vhdl

---
+++ vhdl/hello_world.vhdl
@@ -0,0 +1,19 @@
+-- source https://ghdl.readthedocs.io/en/latest/quick_start/hello/README.html
+
+--  Hello world program
+use std.textio.all; -- Imports the standard textio package.
+
+--  Defines a design entity, without any ports.
+entity hello_world is
+end hello_world;
+
+architecture behaviour of hello_world is
+begin
+  process
+    variable l : line;
+  begin
+    write (l, String'("Hello world!"));
+    writeline (output, l);
+    wait;
+  end process;
+end behaviour;
