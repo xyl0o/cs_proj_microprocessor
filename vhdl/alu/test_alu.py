50cc87cc3ec2f324757cab895081cbb63bbb490b
Maximilian Bauregger <maximilian.bauregger@studium.uni-hamburg.de>
2021-01-21 12:53:53 +0100

    Use python native binary numbers to set alu_op_code

--- vhdl/alu/test_alu.py
+++ vhdl/alu/test_alu.py
@@ -57,61 +57,61 @@ async def in_wait_out_assert(dut, in_ports, out_ports, wait):
 
 
 # @cocotb.test()
-# async def test_ADD(dut)
-#     alu_op_code = "00001"
+# async def test_ADD(dut):
+#     alu_op_code = 0b00001
 
 
 # @cocotb.test()
-# async def test_ADC(dut)
-#     alu_op_code = "00010"
+# async def test_ADC(dut):
+#     alu_op_code = 0b00010
 
 
 # @cocotb.test()
-# async def test_SBC(dut)
-#     alu_op_code = "00011"
+# async def test_SBC(dut):
+#     alu_op_code = 0b00011
 
 
 # @cocotb.test()
-# async def test_SL(dut)
-#     alu_op_code = "00100"
+# async def test_SL(dut):
+#     alu_op_code = 0b00100
 
 
 # @cocotb.test()
-# async def test_SRA(dut)
-#     alu_op_code = "00101"
+# async def test_SRA(dut):
+#     alu_op_code = 0b00101
 
 
 # @cocotb.test()
-# async def test_SRL(dut)
-#     alu_op_code = "00110"
+# async def test_SRL(dut):
+#     alu_op_code = 0b00110
 
 
 # @cocotb.test()
-# async def test_AND(dut)
-#     alu_op_code = "00111"
+# async def test_AND(dut):
+#     alu_op_code = 0b00111
 
 
 # @cocotb.test()
-# async def test_ORR(dut)
-#     alu_op_code = "01000"
+# async def test_ORR(dut):
+#     alu_op_code = 0b01000
 
 
 # @cocotb.test()
-# async def test_XOR(dut)
-#     alu_op_code = "01001"
+# async def test_XOR(dut):
+#     alu_op_code = 0b01001
 
 
 # @cocotb.test()
-# async def test_CMPEQ(dut)
-#     alu_op_code = "01010"
+# async def test_CMPEQ(dut):
+#     alu_op_code = 0b01010
 
 
 # @cocotb.test()
-# async def test_CMPGT(dut)
-#     alu_op_code = "01011"
+# async def test_CMPGT(dut):
+#     alu_op_code = 0b01011
 
 
 # @cocotb.test()
-# async def test_IDOP2(dut)
-#     alu_op_code = "01100"
+# async def test_IDOP2(dut):
+#     alu_op_code = 0b01100
 
