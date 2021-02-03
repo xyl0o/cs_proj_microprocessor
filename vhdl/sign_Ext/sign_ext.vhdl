a95b2700640634d22f989a6073c4d2adf9177025
7mueller <7mueller@informatik.uni-hamburg.de>
2021-02-03 21:03:50 +0100

    completes the sgn_ext but with flaws

--- vhdl/sign_Ext/sign_ext.vhdl
+++ vhdl/sign_Ext/sign_ext.vhdl
@@ -7,10 +7,29 @@ use ieee.numeric_std.all;
 -- to a 32 signed bit value
 entity sign_ext is
 	generic (
-		data_len: integer -- data width
+		data_len: integer := 32 -- data width
 	);
 	port (
 		imm_in: in t_op_imm;
 		op_out: out t_data
 	);
 end sign_ext;
+
+
+variable u_imm_in: unsigned(imm_in);
+variable tmp_result: unsigned(data_len downto 0);
+
+architecture behave of sign_ext is
+
+begin
+ process(Input)
+
+variable u_imm_in: unsigned(imm_in);
+variable tmp_result: unsigned(data_len downto 0);
+
+	temp_result <= '0000000000000000' & u_imm_in;
+	op_out <= std_logic_vector(temp_result);
+
+
+end process;
+end behave;
