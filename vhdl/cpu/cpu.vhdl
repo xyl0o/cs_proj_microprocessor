2e460314f8d908c97f28402e994292cf846a18ad
xyl0o <xyl0o@posteo.net>
2021-02-06 16:09:28 +0100

    Move component def out of cpu.vhdl

--- vhdl/cpu/cpu.vhdl
+++ vhdl/cpu/cpu.vhdl
@@ -17,65 +17,23 @@ entity cpu is
         );
         port (
             clk: in std_logic;
-            data_in: in std_logic_vector(data_len-1 downto 0);
-            instr_in: in std_logic_vector(data_len-1 downto 0);
 
-            data_out: out std_logic_vector(data_len-1 downto 0);
-            data_addr_out: out std_logic_vector(data_len-1 downto 0);
-            instr_addr_out: out std_logic_vector(data_len-1 downto 0);
+            -- instruction memory
+            instr_in: in t_data;
+
+            instr_addr_out: out t_data;
+
+            -- data memory
+            data_in: in t_data;
+
+            data_out: out t_data;
+            data_addr_out: out t_data;
             data_we: out std_logic
 
         );
 end cpu;
 
 architecture cpu_arc of cpu is
-
-    component alu
-        port(
-            alu_op_code: in t_alu_op_code;
-
-            op_1: in std_logic_vector(data_len - 1 downto 0);
-		    op_2: in std_logic_vector(data_len - 1 downto 0);
-		    carryin: in std_logic;
-
-		    result: out std_logic_vector(data_len - 1 downto 0);
-		    carryout: out std_logic;
-		    overflow: out std_logic;
-            compare: out std_logic
-        );
-    end component;
-
-    component decoder
-        port(
-            instr: in t_data;
-            op_code: out t_op_code
-            alu_op_sel: out t_alu_op_sel;
-            reg_select_1: out t_reg_sel;
-            reg_select_2: out t_reg_sel;
-            immediate: out t_op_imm;
-            op2_sel: out std_logic
-        );
-    end component;
-
-    component register_files 
-        port(
-            reg_data_1: in t_data;
-            reg_out_1: out t_data;
-            reg_select_1: in t_reg_sel;
-            reg_write_1_enable: in std_logic;
-
-            reg_data_2: in t_data;
-            reg_out_2: out t_data;
-            reg_select_2: in t_reg_sel;
-            reg_write_2_enable: in std_logic;
-
-            reg_data_3: in t_data;
-            reg_out_3: out t_data;
-            reg_select_3: in t_reg_sel;
-            reg_write_3_enable: in std_logic
-
-        );
-    end component;
 
     signal PC : t_data;
     signal flag_carry:  std_logic;
