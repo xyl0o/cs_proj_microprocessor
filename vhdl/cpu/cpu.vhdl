e4c71d00dccc97e4f3dc0c625184303c6f3bba81
xyl0o <xyl0o@posteo.net>
2021-02-06 12:37:25 +0100

    Some cpu code

--- vhdl/cpu/cpu.vhdl
+++ vhdl/cpu/cpu.vhdl
@@ -41,14 +41,6 @@ architecture cpu_arc of cpu is
         );
     end component;
 
-    component sign_ext 
-        port(
-            imm_in: in t_op_imm;
-            op_out: out t_data
-     
-        );
-    end component;
-
     component decoder 
         port(
             instr: in t_data;
@@ -63,52 +55,126 @@ architecture cpu_arc of cpu is
 
     component register_files 
         port(
-            reg_select_1: in t_reg_sel;
-            reg_select_2: in t_reg_sel;
             reg_data_1: in t_data;
-            reg_data_2: in t_data;
-            reg_write_1_enable: in std_logic;
-            reg_write_2_enable: in std_logic
             reg_out_1: out t_data;
-            reg_out_2: out t_data
+            reg_select_1: in t_reg_sel;
+            reg_write_1_enable: in std_logic;
+
+            reg_data_2: in t_data;
+            reg_out_2: out t_data;
+            reg_select_2: in t_reg_sel;
+            reg_write_2_enable: in std_logic;
+
+            reg_data_3: in t_data;
+            reg_out_3: out t_data;
+            reg_select_3: in t_reg_sel;
+            reg_write_3_enable: in std_logic
 
         );
     end component;
 
-    component pc_select
-        port(
-            op_code: in t_op_code;
-            next_seq_pc: in t_data;
-            alu_result: in t_data;
+    signal PC : t_data;
 
-            next_pc: out t_data
+    -- fetch
+    signal fetch_cmd         : t_data;
+    signal fetch_next_seq_pc : t_data;
 
-        );
-    end component;
-    
-    
-    component adder
-        port(
-            op_1: in t_data;
-            op_2: in t_data;
-            op_out: out t_data
+    --inst_decode
+    signal indec_op_code     : t_op_code;
+    signal indec_op_sel      : t_alu_op_sel;
+    signal indec_target      : t_reg_sel;
+    signal indec_datastore   : t_data;
+    signal indec_op_1        : t_data;
+    signal indec_op_2        : t_data;
+    signal indec_flags_comp  : std_logic;
+    signal indec_flags_carry : std_logic;
+    signal indec_flags_of    : std_logic;
+    signal indec_next_seq_pc : t_data;
 
-        );
-    end component;
+    --execute
+    signal exec_op_code      : t_op_code;
+    signal exec_target       : t_data;
+    signal exec_datastore    : t_data;
+    signal exec_result       : t_data;
+    signal exec_flags_comp   : std_logic;
+    signal exec_flags_carry  : std_logic;
+    signal exec_flags_of     : std_logic;
+    signal exec_next_seq_pc  : t_data;
+
+    --mem_access
+    signal macc_op_code      : t_op_code;
+    signal macc_target       : t_data;
+    signal macc_result       : t_data;
+    signal macc_flags_comp   : std_logic;
+    signal macc_flags_carry  : std_logic;
+    signal macc_flags_of     : std_logic;
+    signal macc_next_seq_pc  : t_data;
+
+    --write_back
 
 
-    component pipeline_reg
-        port(
-            clk: in std_logic;
-            data_in: in std_logic_vector(data_len -1 downto 0);
-            
-            data_out: out std_logic_vector(data_len -1 downto 0)
+begin
 
-            );
-    end component;
+    fetch: process (clk) is
+        if risingEdge then
+            fetch_cmd <= PC;
+            fetch_next_seq_pc <= std_logic_vector(unsigned(PC) + 4);
+        end if;
+    begin
+    end process fetch;
+
+    inst_decode: process is
+    begin
+        if risingEdge then
+            decode.instr <= instr;
+
+            wait;
+
+            register_files.reg_select_1 <= decode.reg_select_1;
+            register_files.reg_select_2 <= decode.reg_select_2;
+
+            wait;
+
+            if decoder.op2_sel then
+                indec_op_2 <= register_files.reg_out_2;
+            else
+                -- sign extend
+                indec_op_2 <= sign_extend(decode.immediate);
 
 
+            indec_op_code <= decoder.op_code;
+            indec_op_sel <= decoder.alu_op_sel;
+            indec_target <= ;
+            indec_datastore <= ;
+            indec_op_1 <= ;
+            indec_op_2 <= ;
+            indec_flags_comp <= ;
+            indec_flags_carry <= ;
+            indec_flags_of <= ;
+            indec_next_seq_pc <= ;
+        end if;
+    end process decode;
 
+    execute: process is
+    begin
+        if risingEdge then
+
+        end if;
+    end process execute;
+
+    mem_access: process is
+    begin
+        if risingEdge then
+
+        end if;
+    end process mem_access;
+
+    write_back: process is
+    begin
+        if risingEdge then
+
+        end if;
+    end process write_back;
 
 begin
     
