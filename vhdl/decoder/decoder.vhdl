b2c0c13db48096f49521f71e8f69b39f693eaa05
xyl0o <xyl0o@posteo.net>
2021-02-10 20:03:28 +0100

    Whitespace

--- vhdl/decoder/decoder.vhdl
+++ vhdl/decoder/decoder.vhdl
@@ -5,24 +5,24 @@ use work.alu_pkg.all;
 use work.decoder_pkg.all;
 
 entity decoder is
-  generic (
-    data_len : positive := 32
-  );
-  port(
-    -- Inputs
-    instr : in std_logic_vector(data_len - 1 downto 0);
+    generic (
+        data_len : positive := 32
+    );
+    port(
+        -- Inputs
+        instr : in std_logic_vector(data_len - 1 downto 0);
 
-    -- Outputs
-    op_code      : out t_op_code;
-    alu_op_sel   : out t_alu_op_code;
-    reg_select_1 : out t_reg_addr;
-    reg_select_2 : out t_reg_addr;
-    reg_select_3 : out t_reg_addr;
-    reg_target   : out t_reg_addr;
-    immediate    : out t_op_imm;
-    op2_sel      : out std_logic; -- 1 if immediate
-    write_en     : out std_logic  -- 1 if write in register
-  );
+        -- Outputs
+        op_code      : out t_op_code;
+        alu_op_sel   : out t_alu_op_code;
+        reg_select_1 : out t_reg_addr;
+        reg_select_2 : out t_reg_addr;
+        reg_select_3 : out t_reg_addr;
+        reg_target   : out t_reg_addr;
+        immediate    : out t_op_imm;
+        op2_sel      : out std_logic; -- 1 if immediate
+        write_en     : out std_logic  -- 1 if write in register
+    );
 end entity decoder;
 
 architecture decoder_arc of decoder is
