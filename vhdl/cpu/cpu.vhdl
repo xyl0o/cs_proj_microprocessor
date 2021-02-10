ebee8fc8a6fcc1fdd7d95009f5c4baec356a2869
xyl0o <xyl0o@posteo.net>
2021-02-10 03:24:53 +0100

    Remove initialize process

--- vhdl/cpu/cpu.vhdl
+++ vhdl/cpu/cpu.vhdl
@@ -132,15 +132,6 @@ begin
     debug_pc <= register_file(to_integer(unsigned(reg_addr_pc)));
     debug_link <= register_file(to_integer(unsigned(reg_addr_link)));
 
-    initialize: process is
-    begin
-        wait for 5 ns;
-        register_file(to_integer(unsigned(reg_addr_zero))) <= (others => '0');
-        register_file(to_integer(unsigned(reg_addr_flags))) <= (others => '0');
-        register_file(to_integer(unsigned(reg_addr_pc))) <= (others => '0');
-        wait;
-    end process initialize;
-
     fetch: process (clk) is
         variable pc : t_data;
     begin
